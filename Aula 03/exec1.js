687de0b76266a1041024237ec171a04bb1d8f98d
Dori Minante <dorim.dev@gmail.com>
2026-04-30 21:30:37 -0300

    aula03

---
+++ Aula 03/exec1.js
@@ -0,0 +1,26 @@
+//console.log("Hello, World!");
+//console.log(2 + 5);
+// comentar = selecionar tudo ctrl + ;
+// let pertence a um escopo de bloco, ou seja, só existe dentro do bloco onde foi declarado. Posso trocar os valores de uma variável declarada com let, mas não posso redeclarar a variável.
+
+let nome = "Guilherme"; // declaração de uma variavel do tipo string
+let numero = 25;
+let boolean = true; // use quando o valor precisa mudar
+const curso = "Full Stack"; // use quando o valor não muda
+
+//const significa que a variável é constante, ou seja, não pode ser reatribuída. O valor de uma variável declarada com const não pode ser alterado, mas se for um objeto ou array, suas propriedades ou elementos podem ser modificados.
+
+if (true) {
+  let mensagem = "Olá";
+  const curso2 = "Jornalismo";
+  console.log(mensagem);
+  console.log(curso2);
+}
+
+// 1. Desativar Sugestões Automáticas (IntelliSense)Isso impede que o VS Code sugira código enquanto você digita.Abra as configurações (clique na engrenagem no canto inferior esquerdo e selecione Settings ou use Ctrl + ,).Pesquise por quickSuggestions.Desmarque a opção Editor: Quick Suggestions (ou defina como false no JSON).
+
+// 2. Desativar Sugestões de ParâmetrosIsso impede que o VS Code mostre dicas de parâmetros ao chamar funções.Abra as configurações.Pesquise por parameterHints.Desmarque a opção Editor: Parameter Hints (ou defina como false no JSON).
+
+// 3. Desativar Sugestões de ImportaçãoIsso impede que o VS Code sugira automaticamente importações ao usar símbolos não definidos.Abra as configurações.Pesquise por autoImportSuggestions.Desmarque a opção JavaScript: Auto Import Suggestions (ou defina como false no JSON).
+
+// https://www.youtube.com/watch?v=TyzFP3XP7Fs&t=42s
